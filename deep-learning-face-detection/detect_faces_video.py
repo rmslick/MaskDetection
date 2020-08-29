b9aad41094dfad0f9c3588d31a4b5c51a4a74d26
Max Radecki <maxradecki43@gmail.com>
2020-08-29 12:51:07 -0400

    removed_args

--- deep-learning-face-detection/detect_faces_video.py
+++ deep-learning-face-detection/detect_faces_video.py
@@ -9,19 +9,9 @@ import imutils
 import time
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
-
 # load our serialized model from disk
 print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+net = cv2.dnn.readNetFromCaffe('deploy.prototxt.txt', 'res10_300x300_ssd_iter_140000.caffemodel')
 
 # initialize the video stream and allow the cammera sensor to warmup
 print("[INFO] starting video stream...")
@@ -53,7 +43,7 @@ while True:
 
 		# filter out weak detections by ensuring the `confidence` is
 		# greater than the minimum confidence
-		if confidence < args["confidence"]:
+		if confidence < .5:
 			continue
 
 		# compute the (x, y)-coordinates of the bounding box for the
