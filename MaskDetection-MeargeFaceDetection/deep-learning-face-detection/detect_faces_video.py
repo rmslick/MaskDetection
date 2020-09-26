34ab5ca56cb40fe7bb01a712bd1ff0173b79df4a
Kartik Yendamuri <kartikyendamuri@KartiksMacbook.local>
2020-09-26 12:41:02 -0400

    Final Version

---
+++ MaskDetection-MeargeFaceDetection/deep-learning-face-detection/detect_faces_video.py
@@ -0,0 +1,140 @@
+# USAGE
+# python detect_faces_video.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
+
+# import the necessary packages
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import tensorflow.keras
+from PIL import Image, ImageOps
+from gtts import gTTS
+from playsound import playsound
+import os
+def wearmask():
+    mytext = 'You are not wearing a mask. Please take one from the opening box.'
+    language = 'en'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save("wearmask.mp3")
+    playsound('wearmask.mp3')
+
+def proceed():
+    mytext = 'Thank you for wearing a mask. You may proceed.'
+    language = 'en'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save("proceed.mp3")
+    playsound('proceed.mp3')
+
+def classification():
+	# Disable scientific notation for clarity
+	np.set_printoptions(suppress=True)
+
+	# Load the model
+	model = tensorflow.keras.models.load_model('keras_model.h5')
+
+	# Create the array of the right shape to feed into the keras model
+	# The 'length' or number of images you can put into the array is
+	# determined by the first position in the shape tuple, in this case 1.
+	data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+	# Replace this with the path to your image
+	image = Image.open('frame.jpg')
+
+	#resize the image to a 224x224 with the same strategy as in TM2:
+	#resizing the image to be at least 224x224 and then cropping from the center
+	size = (224, 224)
+	image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+	#turn the image into a numpy array
+	image_array = np.asarray(image)
+
+	# display the resized image
+	#image.show()
+
+	# Normalize the image
+	normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+	# Load the image into the array
+	data[0] = normalized_image_array
+
+	# run the inference
+	prediction = model.predict(data)
+	if prediction[0][0]==max(prediction[0]):
+    	print("Mask")
+	elif prediction[0][1]==max(prediction[0]):
+    	print("No mask")
+		print(prediction[0][0])
+		print(prediction)
+		wearmask()
+		proceed()
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe('deploy.prototxt.txt', 'res10_300x300_ssd_iter_140000.caffemodel')
+
+# initialize the video stream and allow the cammera sensor to warmup
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+ 
+	# grab the frame dimensions and convert it to a blob
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+		(300, 300), (104.0, 177.0, 123.0))
+ 
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	net.setInput(blob)
+	detections = net.forward()
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with the
+		# prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence < .5:
+			continue
+
+		# compute the (x, y)-coordinates of the bounding box for the
+		# object
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+ 
+		# draw the bounding box of the face along with the associated
+		# probability
+		text = "{:.2f}%".format(confidence * 100)
+		y = startY - 10 if startY - 10 > 10 else startY + 10
+		cv2.rectangle(frame, (startX, startY), (endX, endY),
+			(0, 0, 255), 2)
+		cv2.putText(frame, text, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+		#cropping the rest of the body out of the frame to only show the face.
+		crop_image = frame[startY:endY,startX:endX]
+		#display image to screen
+		cv2.imshow("face",frame)
+		#shaving extracted face
+		cv2.imwrite("frame.jpg" , crop_image)
+		classification()
+	# show the output frame
+	#cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
